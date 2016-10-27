f0a57adf688f0b78234df9e8cd1a616727408930
ki33520 <ki33520@gmail.com>
2016-10-27 17:24:42 +0800

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,12 +35,49 @@ $('.popup').each(function(){
 	$(this).find('.popup-close').on('click',hidePopUp);
 	$(this).find('.btn-close').on('click',hidePopUp);
 });
-
-$('.form-withdraw .btn-submit').click(function(){
+$('#withdraw-btn').on('click',function(){
 	var sum = $.trim($('#sum').val());
-	if(sum===''){
+	if(sum==='' || Number(sum) < 50){
 		var popup = $(this).data('popup');
 		$(popup).show();
 		return false;
 	}
+})
+$('#apply-btn').on('click',function(){
+	var self = this;
+	var popup = $(this).data('popup');
+	$(popup).show();
+	setTimeout(function(){
+		window.location.href = $(self).attr('href');
+	},2000)
+	return false;
+});
+$('.btn-buy').on('click',function(){
+	$('.product-buy').show();
+});
+$('.product-buy .close').on('click',function(){
+	$('.product-buy').hide();
+});
+$('.add-sub').each(function(){
+	var sub = $(this).find('.btn-sub'),
+		add = $(this).find('.btn-add'),
+		number = $(this).find('.number'),
+		num = Number(number.text()),
+		max = $(this).data('max');
+	function count(){
+		num = num + this;
+		sub.removeClass('disabeld');
+		add.removeClass('disabeld');
+		if(num<=0){
+			num = 0;
+			sub.addClass('disabeld');
+		}
+		if(num>=max){
+			num = max;
+			add.addClass('disabeld');
+		}
+		number.text(num);
+	}
+	sub.on('click',count.bind(-1));
+	add.on('click',count.bind(1));
 })
